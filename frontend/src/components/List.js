966bf4c9bd55c20896b0ca7b39493b818b5a8f78
meawhung <meawhung@gmail.com>
2021-04-01 15:30:19 +0900

    merge with backend

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -4,27 +4,11 @@ import { Container,Card,CardDeck,Modal,Button} from "react-bootstrap";
 
 const ImageCat = (props) =>{
     return(
-        <img width="300px" src={props.path} alt={props.name} />
+        <img width="300px" src={props.imgB64} alt={props.name} />
     )
    
 }
-const CardCat =(props) =>{
-    const [modalShow, setModalShow] = React.useState(false);
-    return(
-        <Card>
-            <Button variant="primary" onClick={() => setModalShow(true)}>
-            <ImageCat path={props.path} name={props.name} />
-            </Button>
-            <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />            
-            <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            </Card.Body>
-        </Card>
-    )
-}
+
 
 
 function MyVerticallyCenteredModal(props) {
@@ -37,7 +21,7 @@ function MyVerticallyCenteredModal(props) {
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+            {props.cat.breed_name}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -55,21 +39,43 @@ function MyVerticallyCenteredModal(props) {
     );
   }
 
+  const CardCat =(props) =>{
+    const [modalShow, setModalShow] = React.useState(false);
+    console.log('sd')
+    console.log(props.cat)
+    return(
+        <Card>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+              <ImageCat imgB64={props.cat.image} name={props.cat.breed_name} />
+            </Button>
+            <MyVerticallyCenteredModal
+                cat = {props.cat}
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />            
+          <Card.Body>
+            <Card.Title>{props.cat.breed_name}</Card.Title>
+          </Card.Body>
+        </Card>
+    )
+}
 
-
-const List = () =>{
-
+const List = (props) =>{
     return(
     <Container>
-        <CardDeck>
-            <CardCat path="image/black_bombay_cat.jpg" name="black_bombay" title="Black Bombay"/>
-            <CardCat path="image/burmese_cat.jpg" name="burmese" title="Burmese"/>
-            <CardCat path="image/khao_manee_cat.jpg" name="khao_manee" title="Khao Manee"/>
+      <CardDeck>
+
+      {props.cats.map((cat) => (
+         <CardCat cat={cat} />
+        ))}
+            
+            {/* <CardCat cat={props.cats[1]} />
+            <CardCat cat={props.cats[2]} />
         </CardDeck>
         <br></br>
         <CardDeck>
-            <CardCat path="image/korat_cat.jpg" name="korat_cat" title="Korat"/>
-            <CardCat path="image/siamese_cat.jpg" name="siamese_cat" title="Siamese"/>            
+            <CardCat cat={props.cats[3]} path="image/korat_cat.jpg" name="korat_cat" title="Korat"/>
+            <CardCat cat={props.cats[4]} path="image/siamese_cat.jpg" name="siamese_cat" title="Siamese"/>             */}
         </CardDeck>
     </Container>
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar.js'
 import List from './components/List.js'
 import Title from './components/Title.js'
-class Cat extends React.Component {
-  render() {
+
+
+const Cat = () => {
+  const [cats, setCats] = useState([])
+  const fetchCats = async () => {
+    const response = await fetch("http://localhost:8000/cat")
+    const cats = await response.json()
+
+    setCats(cats.data[0])
+  }
+
+  useEffect(() => {
+    fetchCats()
+  }, [])
+
     return (
       <div>
+       
         <Navbar />
         <Title />
-        <List />
+        <List cats={cats}/>
+  
+        
       </div>
     );
-  }
 }
 
 ReactDOM.render(<Cat />, document.getElementById('root'));
+
+/* <TodosContext.Provider value={{todos, fetchTodos}}>
+    <Stack spacing={5}>
+      {todos.map((todo) => (
+        <b>{todo.item}</b>
+      ))}
+    </Stack>
+  </TodosContext.Provider> */
