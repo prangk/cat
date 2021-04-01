611745289836a3864c057a448cd45e2607d9ea00
prangk <80370589+prangk@users.noreply.github.com>
2021-04-01 14:38:44 +0900

    Update List.js

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import { Container, Row, Col} from "react-bootstrap";
+import { Container,Card,CardDeck} from "react-bootstrap";
 
 
 const ImageCat = (props) =>{
     return(
-        <Col><img width="300px" src={props.path} alt={props.name} /></Col>
+        <img width="300px" src={props.path} alt={props.name} />
     )
    
 }
 
+
 const List = () =>{
     return(
     <Container>
-    <Row>
-        <ImageCat path="image/black_bombay_cat.jpg" name="black_bombay" />
-        <ImageCat path="image/burmese_cat.jpg" name="burmese" />
-        <ImageCat path="image/khao_manee_cat.jpg" name="khao_manee" />
-        <ImageCat path="image/korat_cat.jpg" name="korat_cat" />
-        <ImageCat path="image/siamese_cat.jpg" name="siamese" />
-
-    </Row>
+        <CardDeck>
+        <Card>
+            <ImageCat path="image/black_bombay_cat.jpg" name="black_bombay" />
+            <Card.Body>
+            <Card.Title>Black Bombay</Card.Title>
+            </Card.Body>
+        </Card>
+        <Card>
+            <ImageCat path="image/burmese_cat.jpg" name="burmese" />
+            <Card.Body>
+            <Card.Title>Burmese</Card.Title>
+            </Card.Body>
+        </Card>
+        <Card>
+            <ImageCat path="image/khao_manee_cat.jpg" name="khao_manee" />
+            <Card.Body>
+            <Card.Title>Khao Manee</Card.Title>
+            </Card.Body>
+        </Card>
+        </CardDeck>
+        <br></br>
+        <CardDeck>
+        <Card>
+            <ImageCat path="image/korat_cat.jpg" name="korat_cat" />
+            <Card.Body>
+            <Card.Title>Korat</Card.Title>
+            </Card.Body>
+        </Card>
+        <Card>
+            <ImageCat path="image/siamese_cat.jpg" name="siamese" />       
+            <Card.Body>
+            <Card.Title>Siamese</Card.Title>
+            </Card.Body>
+        </Card>
+        </CardDeck>
     </Container>
     );
 };
