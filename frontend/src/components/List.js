c52df1c284e68af6b85fdae8421e8ac915829a82
prangk <thunchanok.prang@gmail.com>
2021-04-01 16:10:28 +0900

    cat list

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,25 +1,77 @@
 import React from 'react';
-import { Container, Row, Col} from "react-bootstrap";
+import { Container,Card,CardDeck,Modal,Button,CardColumns} from "react-bootstrap";
 
 
 const ImageCat = (props) =>{
     return(
-        <Col><img width="300px" src={props.path} alt={props.name} /></Col>
+        <img width="300px" src={props.path} alt={props.name} />
     )
    
 }
+const CardCat =(props) =>{
+    const [modalShow, setModalShow] = React.useState(false);
+    return(
+        <Card>
+            <Button variant="default" style={{ color: "white", background: "white" }} onClick={() => setModalShow(true)}>
+            <ImageCat path={props.path} name={props.name} />
+            </Button>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />            
+            <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            </Card.Body>
+        </Card>
+    )
+}
+
+
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+
 
 const List = () =>{
+
     return(
     <Container>
-    <Row>
-        <ImageCat path="image/black_bombay_cat.jpg" name="black_bombay" />
-        <ImageCat path="image/burmese_cat.jpg" name="burmese" />
-        <ImageCat path="image/khao_manee_cat.jpg" name="khao_manee" />
-        <ImageCat path="image/korat_cat.jpg" name="korat_cat" />
-        <ImageCat path="image/siamese_cat.jpg" name="siamese" />
-
-    </Row>
+        <CardDeck>
+            <CardCat path="image/black_bombay_cat.jpg" name="black_bombay" title="Black Bombay"/>
+            <CardCat path="image/burmese_cat.jpg" name="burmese" title="Burmese"/>
+            <CardCat path="image/khao_manee_cat.jpg" name="khao_manee" title="Khao Manee"/>
+            <CardCat path="image/korat_cat.jpg" name="korat_cat" title="Korat"/>
+            <CardCat path="image/siamese_cat.jpg" name="siamese_cat" title="Siamese"/>  
+        </CardDeck>
+        <br></br>
+        <CardDeck>
+          
+        </CardDeck>
     </Container>
     );
 };
--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -4,7 +4,7 @@ const Title = () =>{
     return(
     <div class="jumbotron">
         <h1 class="display-4">แมวไทย</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p class="lead">This is the information of thai cat.</p>
         <hr class="my-4" />
     </div>
     )
