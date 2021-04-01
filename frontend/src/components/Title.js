0a4b52346da5de67239529104763edc9a02b790d
meawhung <meawhung@gmail.com>
2021-04-01 13:40:21 +0900

    push frontend

---
+++ frontend/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () =>{
+    return(
+    <div class="jumbotron">
+        <h1 class="display-4">แมวไทย</h1>
+        <p class="lead">This is the information of thai cat.</p>
+        <hr class="my-4" />
+    </div>
+    )
+}
+
+export default Title;
